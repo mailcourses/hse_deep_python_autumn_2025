0209537d02085eccc89b4f1330512ef21c7a93a4
Ryabkov Igor <i.ryabkov@s3.infra-prod.one-infra.ru>
2025-10-24 17:28:21 +0300

    Add hw5

---
+++ hw-05/test_task5.py
@@ -0,0 +1,18 @@
+from task5 import LRUCache
+
+
+def test_predict_general_cases():
+    cache = LRUCache(2)
+
+    cache.set("k1", "val1")
+    cache.set("k2", "val2")
+
+    assert cache.get("k3") is None
+    assert cache.get("k2") == "val2"
+    assert cache.get("k1") == "val1"
+
+    cache.set("k3", "val3")
+
+    assert cache.get("k3") == "val3"
+    assert cache.get("k2") is None
+    assert cache.get("k1") == "val1"
